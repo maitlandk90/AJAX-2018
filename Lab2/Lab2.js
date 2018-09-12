c34eadebea6e1d1c35617acc3b89b90ed5aa07e6
Kyle Maitland <35706464+maitlandk90@users.noreply.github.com>
2018-09-12 14:01:53 -0500

    Lab 2
    Simple console outputs with local and globe variables

---
+++ Lab2/Lab2.js
@@ -0,0 +1,19 @@
+let labTitle = "Programming for Designers - Lab 2"
+
+let printLabTitle = function() {
+  console.log(labTitle);
+}
+
+let init = function() {
+
+  let id = 10;
+  let name = "Kyle Maitland";
+  let borninMadison = false;
+
+  console.log(id);
+  console.log(name);
+  console.log(borninMadison);
+
+  printLabTitle();
+
+}
